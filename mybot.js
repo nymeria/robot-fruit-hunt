37a97bc28479a24f84c5d933c94699e6737ccb4c
Rebecca <nymeria@github>
2012-06-28 17:34:05 -0700

    greedy bot

--- mybot.js
+++ mybot.js
@@ -1,22 +1,76 @@
 function new_game() {
 }
 
-function make_move() {
-   var board = get_board();
+function make_move() {	
+	var board = get_board();
+	var currentPositionX = get_my_x();
+	var currentPositionY = get_my_y();
 
-   // we found an item! take it!
-   if (board[get_my_x()][get_my_y()] > 0) {
-       return TAKE;
-   }
+	if(shouldTakeFruit(board[currentPositionX][currentPositionY])){
+		return TAKE;
+    }
 
-   var rand = Math.random() * 4;
+	var closestFruit = findFruit(currentPositionX, currentPositionY,board);
+	var moveHorizontal = currentPositionX-closestFruit.x;
+	if(moveHorizontal > 0){
+		return WEST;
+	}else if(moveHorizontal < 0){
+		return EAST;
+	}
 
-   if (rand < 1) return NORTH;
-   if (rand < 2) return SOUTH;
-   if (rand < 3) return EAST;
-   if (rand < 4) return WEST;
+	var moveVertical = currentPositionY-closestFruit.y;
+	if(moveVertical > 0){
+		return NORTH;
+	}else if(moveVertical < 0){
+		return SOUTH;
+	}
+}
 
-   return PASS;
+function shouldTakeFruit(fruitType){
+	if(fruitType > 0){
+		var majority = Math.ceil(	get_total_item_count(fruitType)/2);
+		if(get_my_item_count(fruitType)	< majority){
+			return true;
+		}
+	}
+	return false;
+}
+
+function findFruit(myX, myY, board){
+	var queue = new Array();
+	var result;
+	queue.push(new cell(myX,myY));
+	var index=0;
+	while(index < queue.length ){
+		var position = queue[index];
+		var fruitType = board[position.x][position.y];
+		if(shouldTakeFruit(fruitType)){
+			return position;
+		}
+
+		addNeighbors(position, queue);
+		index++;
+	}
+}
+
+function addNeighbors(position, queue){
+	if(position.x-1>=0){
+		queue.push(new cell(position.x-1, position.y));
+	}
+	if(position.x+1<WIDTH){
+		queue.push(new cell(position.x+1, position.y));
+	}
+	if(position.y-1>=0){
+		queue.push(new cell(position.x, position.y-1));
+	}
+	if(position.y+1<HEIGHT){
+		queue.push(new cell(position.x, position.y+1));
+	}
+}
+
+function cell(x,y){
+	this.x = x;
+	this.y = y;
 }
 
 // Optionally include this function if you'd like to always reset to a 
